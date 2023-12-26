bde04b7d6d84e11497a3283a8493d840681941de
Sherdan <sherdan659@gmail.com>
2023-12-26 18:02:36 +0000

    Fixed a few details

--- Assignment/resources/views/layouts/main.blade.php
+++ Assignment/resources/views/layouts/main.blade.php
@@ -19,7 +19,7 @@
     <nav class="navbar navbar-expand-lg navbar-light">
       <div class="container">
         <a class="navbar-brand text-uppercase" href="index.html">            
-            <strong>Contact</strong> App
+            <strong>CAROZZA APP</strong>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-toggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
